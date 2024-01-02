d6b67a12fbc783bf26832387c454733e4d409554
cdpath <cdpath@outlook.com>
2024-01-02 19:47:06 +0800

    add send email

---
+++ send_email.py
@@ -0,0 +1,27 @@
+import smtplib
+import os
+from email.message import EmailMessage
+
+
+email_user = os.environ.get('EMAIL_USER')
+email_password = os.environ.get('EMAIL_PASSWORD')
+kindle_email = os.environ.get('KINDLE_EMAIL')
+
+
+msg = EmailMessage()
+msg['Subject'] = 'LeetCode Question'
+msg['From'] = email_user
+msg['To'] = kindle_email
+msg.set_content('Please find the attached LeetCode question.')
+
+
+file_path = 'LeetCode_Question.pdf'
+with open(file_path, 'rb') as f:
+    file_data = f.read()
+msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename='LeetCode_Question.pdf')
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(email_user, email_password)
+    smtp.send_message(msg)
+
